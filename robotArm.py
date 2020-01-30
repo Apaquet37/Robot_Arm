c42c75ab1189c39f2d593ccc1ee1253e62e3bdcb
Abigail B Paquette <apaquet37@ccs.local>
2020-01-30 09:54:47 -0500

    Code

--- robotArm.py
+++ robotArm.py
@@ -4,6 +4,8 @@ import busio #pylint: disable-msg=import-error
 import pulseio #pylint: disable-msg=import-error
 from analogio import AnalogIn #pylint: disable-msg=import-error
 from adafruit_motor import servo #pylint: disable-msg=import-error
+from digitalio import DigitalInOut, Direction, Pull #pylint: disable-msg=import-error
+
 
 pwm1 = pulseio.PWMOut(board.A2, duty_cycle=2**15, frequency = 50)
 pwm2 = pulseio.PWMOut(board.A3, duty_cycle=2**15, frequency = 50)
@@ -18,6 +20,13 @@ dial1 = AnalogIn(board.D5)
 dial2 = AnalogIn(board.D6)
 dial3 = AnalogIn(board.D7)
 dial4 = AnalogIn(board.D8)
+
+switch = DigitalInOut(board.D13)
+switch.direction = Direction.INPUT
+switch.pull = Pull.UP
+
+if switch.value:
+    controlToggle = 0
 
 def get_voltage(pin):
     return(pin.value*180)/65536
