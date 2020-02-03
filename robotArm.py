79243c472900b899bcb302b098bc9fc975dd8d81
Abigail B Paquette <apaquet37@ccs.local>
2020-02-03 09:56:55 -0500

    Fixed servo and potentiometer pins

--- robotArm.py
+++ robotArm.py
@@ -7,23 +7,25 @@ from adafruit_motor import servo #pylint: disable-msg=import-error
 from digitalio import DigitalInOut, Direction, Pull #pylint: disable-msg=import-error
 
 
-pwm1 = pulseio.PWMOut(board.A2, duty_cycle=2**15, frequency = 50)
-pwm2 = pulseio.PWMOut(board.A3, duty_cycle=2**15, frequency = 50)
-pwm3 = pulseio.PWMOut(board.A4, duty_cycle=2**15, frequency = 50) 
-pwm4 = pulseio.PWMOut(board.D3, duty_cycle=2**15, frequency = 50)
+pwm1 = pulseio.PWMOut(board.D3, duty_cycle=2**15, frequency = 50)
+pwm2 = pulseio.PWMOut(board.D4, duty_cycle=2**15, frequency = 50)
+pwm3 = pulseio.PWMOut(board.D5, duty_cycle=2**15, frequency = 50) 
+pwm4 = pulseio.PWMOut(board.D6, duty_cycle=2**15, frequency = 50)
 servo1 = servo.Servo(pwm1) 
 servo2 = servo.Servo(pwm2)
 servo3 = servo.Servo(pwm3) 
 servo4 = servo.Servo(pwm4)
 
-dial1 = AnalogIn(board.D5)
-dial2 = AnalogIn(board.D6)
-dial3 = AnalogIn(board.D7)
-dial4 = AnalogIn(board.D8)
+dial1 = AnalogIn(board.A1)
+dial2 = AnalogIn(board.A2)
+dial3 = AnalogIn(board.A3)
+dial4 = AnalogIn(board.A4)
 
 switch = DigitalInOut(board.D13)
 switch.direction = Direction.INPUT
 switch.pull = Pull.UP
+
+print("go")
 
 if switch.value:
     controlToggle = 0
@@ -41,3 +43,4 @@ while True:
     servo2.angle = myDial2
     servo3.angle = myDial3
     servo4.angle = myDial4
+    print(myDial1)
